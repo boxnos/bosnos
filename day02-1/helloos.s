488a28e469199e619c7bcfc591f12f8f104a9d51
boxnos <boxnos@yahoo.com>
2019-02-02 18:50:14 +0900

    Day 02-1

---
+++ day02-1/helloos.s
@@ -0,0 +1,60 @@
+  .code16
+
+  jmp entry
+  .byte 0x90
+
+  .ascii "HELLOIPL"
+  .word 512
+  .byte 1
+  .word 1
+  .byte 2
+  .word 224
+  .word 2880
+  .byte 0xf0
+  .word 9
+  .word 18
+  .word 2
+
+  .long 0
+  .long 2880
+  .byte 0x00, 0x00, 0x29
+  .long 0xffffffff
+  .ascii "BOXNOS     "
+  .ascii "FAT12   "
+  .skip 18
+#  . = . + 18
+
+entry:
+  movw $0, %ax
+  movw %ax, %ss
+  movw $0x7c00, %sp
+  movw %ax, %ds
+  movw %ax, %es
+  movw $msg, %si
+# .byte 0x8e, 0xd8, 0x8e
+
+putloop:
+  movb (%si), %al
+  add $1, %si
+  cmp $0, %al
+  je fin
+  movb $0x0e, %ah
+  movw $15, %bx
+  int $0x10
+  jmp putloop
+
+fin:
+  hlt
+  jmp fin
+
+msg:
+  .string "\n\nhello, world\n"
+
+  .org 0x1fe
+  .byte 0x55, 0xaa
+
+  .byte 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
+  //.org . + 4600
+  .skip 4600
+  .byte 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
+  .org 0x200 * 2880
---
+++ day02-1/link.ls
@@ -0,0 +1,6 @@
+OUTPUT_FORMAT("binary");
+IPL_BASE = 0x7c00;
+
+SECTIONS {
+        . = IPL_BASE;
+}
